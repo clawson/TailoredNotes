b7dfc53de50b304b12c1fe5992beb881b06b1b19
rosewebdev <jason@rosewebdev.com>
2015-04-28 14:16:30 -0600

    Added Person segmented View Controller

---
+++ TailoredNotes/PersonSegmentedViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonSegmentedViewController.h
+//  TailoredNotes
+//
+//  Created by Jason Kupiec on 4/28/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonSegmentedViewController : UIViewController
+
+@end
