21c93adfbeaa8b7250513d1efaaaffe278b6e6dc
Dan Clawson <clawson@uen.org>
2015-04-28 11:27:46 -0600

    Add stub project from Xcode.

---
+++ TailoredNotes/TailoredNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TailoredNotes
+//
+//  Created by Dan Clawson on 4/28/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
