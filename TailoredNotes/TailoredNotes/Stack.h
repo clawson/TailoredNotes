ccc5d4e749d12972d446b20310bcfec1fe11ada6
Dan Clawson <clawson@uen.org>
2015-04-28 12:37:30 -0600

    Add Stack class.

---
+++ TailoredNotes/TailoredNotes/Stack.h
@@ -0,0 +1,13 @@
+//
+//  Stack.h
+//  TailoredNotes
+//
+//  Created by Dan Clawson on 4/28/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Stack : NSObject
+
+@end
---
+++ TailoredNotes/TailoredNotes/Stack.m
@@ -0,0 +1,13 @@
+//
+//  Stack.m
+//  TailoredNotes
+//
+//  Created by Dan Clawson on 4/28/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+#import "Stack.h"
+
+@implementation Stack
+
+@end
