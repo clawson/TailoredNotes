88a1e7d2f777610ab7d5f4da27874bfe07f80c88
rosewebdev <jason@rosewebdev.com>
2015-05-02 12:52:36 -0600

    Added new View Controllers

---
+++ TailoredNotes/PeopleAlphaViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeopleAlphaViewController.h
+//  TailoredNotes
+//
+//  Created by Jason Kupiec on 5/2/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeopleAlphaViewController : UIViewController
+
+@end
---
+++ TailoredNotes/PeopleOpenViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeopleOpenViewController.h
+//  TailoredNotes
+//
+//  Created by Jason Kupiec on 5/2/15.
+//  Copyright (c) 2015 Slacker Tools. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeopleOpenViewController : UIViewController
+
+@end
